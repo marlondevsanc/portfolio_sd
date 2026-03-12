bb4677371206245cdf75d60d14d5e22319f92baf
Marlon Sánchez <marlondevsanc@gmail.com>
2026-03-12 17:53:39 -0600

    Add new media files for Valientes y Bellas campaign
    - Added multiple video files (IMG_5328.mp4, IMG_5329.mp4, IMG_5330.mp4, VYB5331.mp4) to the reels directory.
    - Included several new images (valientesybellas1.jpg to valientesybellas6.jpg) for promotional use.

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -4,66 +4,66 @@ export interface VideoItem {
     logo?: string;
 }
 
-import daikareel1 from '../assets/Images/daikastore/reels/1.mp4';
-import daikareel2 from '../assets/Images/daikastore/reels/2.mp4';
-import daykalogo from '../assets/Images/daikastore/logodaykas.jpeg';
+import daikareel1 from '/Images/daikastore/reels/1.mp4';
+import daikareel2 from '/Images/daikastore/reels/2.mp4';
+import daykalogo from '/Images/daikastore/logodaykas.jpeg';
 
 // Fireplaces 
-import fireplacelogo from '../assets/Images/fireplace/logofireplace.jpg'
+import fireplacelogo from '/Images/fireplace/logofireplace.jpg'
 
-import fireplace1 from '../assets/Images/fireplace/reels/Reel 1- Fireplace.mp4'
-import fireplace2 from '../assets/Images/fireplace/reels/Reel 2- Fireplace.mp4'
-import fireplace3 from '../assets/Images/fireplace/reels/Reel 3- Fireplace.mp4'
-import fireplace4 from '../assets/Images/fireplace/reels/Reel 4- Fireplace.mp4'
-import fireplace5 from '../assets/Images/fireplace/reels/Reel 5- Fireplace.mp4'
-import fireplace6 from '../assets/Images/fireplace/reels/Reel 6- Fireplace.mp4'
-import fireplace7 from '../assets/Images/fireplace/reels/Reel 7- Fireplace.mp4'
-import fireplace8 from '../assets/Images/fireplace/reels/Reel 8- Fireplace.mp4'
+import fireplace1 from '/Images/fireplace/reels/Reel 1- Fireplace.mp4'
+import fireplace2 from '/Images/fireplace/reels/Reel 2- Fireplace.mp4'
+import fireplace3 from '/Images/fireplace/reels/Reel 3- Fireplace.mp4'
+import fireplace4 from '/Images/fireplace/reels/Reel 4- Fireplace.mp4'
+import fireplace5 from '/Images/fireplace/reels/Reel 5- Fireplace.mp4'
+import fireplace6 from '/Images/fireplace/reels/Reel 6- Fireplace.mp4'
+import fireplace7 from '/Images/fireplace/reels/Reel 7- Fireplace.mp4'
+import fireplace8 from '/Images/fireplace/reels/Reel 8- Fireplace.mp4'
 
 // Flower Anto
-import logofloweranto from '../assets/Images/floweranto/logofloweranto.jpeg';
-import flowerantoreel from '../assets/Images/floweranto/reels/reel2.mp4';
+import logofloweranto from '/Images/floweranto/logofloweranto.jpeg';
+import flowerantoreel from '/Images/floweranto/reels/reel2.mp4';
 
 // Kensof
-import logokensof from '../assets/Images/kensof/logokensof.jpeg';
-import kensofreel1 from '../assets/Images/kensof/reels/Reel 1- Kensof.mp4';
-import kensofreel2 from '../assets/Images/kensof/reels/Reel 2- Kensof.mp4';
-import kensofreel3 from '../assets/Images/kensof/reels/Reel 3- Kensof.mp4';
-import kensofreel4 from '../assets/Images/kensof/reels/Reel 4- Kensof.mp4';
+import logokensof from '/Images/kensof/logokensof.jpeg';
+import kensofreel1 from '/Images/kensof/reels/Reel 1- Kensof.mp4';
+import kensofreel2 from '/Images/kensof/reels/Reel 2- Kensof.mp4';
+import kensofreel3 from '/Images/kensof/reels/Reel 3- Kensof.mp4';
+import kensofreel4 from '/Images/kensof/reels/Reel 4- Kensof.mp4';
 
 // Dreamers Concept Store
-import logoDreamers from '../assets/Images/dreamers/Logo-doradoblanco.png'
-import dreamersReel1 from '../assets/Images/dreamers/reels/IMG_5331.mp4';
-import dreamersReel2 from '../assets/Images/dreamers/reels/IMG_5332.mp4';
-import dreamersReel3 from '../assets/Images/dreamers/reels/IMG_5333.mp4';
-import dreamersReel4 from '../assets/Images/dreamers/reels/IMG_5334.mp4';
-import dreamersReel5 from '../assets/Images/dreamers/reels/IMG_5335.mp4';
-import dreamersReel6 from '../assets/Images/dreamers/reels/IMG_5336.mp4';
-import dreamersReel7 from '../assets/Images/dreamers/reels/IMG_5337.mp4';
+import logoDreamers from '/Images/dreamers/Logo-doradoblanco.png'
+import dreamersReel1 from '/Images/dreamers/reels/IMG_5331.mp4';
+import dreamersReel2 from '/Images/dreamers/reels/IMG_5332.mp4';
+import dreamersReel3 from '/Images/dreamers/reels/IMG_5333.mp4';
+import dreamersReel4 from '/Images/dreamers/reels/IMG_5334.mp4';
+import dreamersReel5 from '/Images/dreamers/reels/IMG_5335.mp4';
+import dreamersReel6 from '/Images/dreamers/reels/IMG_5336.mp4';
+import dreamersReel7 from '/Images/dreamers/reels/IMG_5337.mp4';
 
 // Valientes y bellas
-import logovalientes from '../assets/Images/valientesybellas/logovalientesybellas.jpg'
-import valientesreel1 from '../assets/Images/valientesybellas/reels/IMG_5328.mp4';
-import valientesreel2 from '../assets/Images/valientesybellas/reels/IMG_5329.mp4';;
-import valientesreel3 from '../assets/Images/valientesybellas/reels/IMG_5330.mp4';
-import valientesreel4 from '../assets/Images/valientesybellas/reels/VYB5331.mp4';
+import logovalientes from '/Images/valientesybellas/logovalientesybellas.jpg'
+import valientesreel1 from '/Images/valientesybellas/reels/IMG_5328.mp4';
+import valientesreel2 from '/Images/valientesybellas/reels/IMG_5329.mp4';;
+import valientesreel3 from '/Images/valientesybellas/reels/IMG_5330.mp4';
+import valientesreel4 from '/Images/valientesybellas/reels/VYB5331.mp4';
 
 // Soludental
 
-import logosoludental from '../assets/Images/soludental/soludentallogo.jpg'
-import soludentalreel1 from '../assets/Images/soludental/reels/IMG_5338.mp4';
-import soludentalreel2 from '../assets/Images/soludental/reels/IMG_5339.mp4';
-import soludentalreel3 from '../assets/Images/soludental/reels/IMG_5340.mp4';
-import soludentalreel4 from '../assets/Images/soludental/reels/IMG_5341.mp4';
-import soludentalreel5 from '../assets/Images/soludental/reels/IMG_5342.mp4';
+import logosoludental from '/Images/soludental/soludentallogo.jpg'
+import soludentalreel1 from '/Images/soludental/reels/IMG_5338.mp4';
+import soludentalreel2 from '/Images/soludental/reels/IMG_5339.mp4';
+import soludentalreel3 from '/Images/soludental/reels/IMG_5340.mp4';
+import soludentalreel4 from '/Images/soludental/reels/IMG_5341.mp4';
+import soludentalreel5 from '/Images/soludental/reels/IMG_5342.mp4';
 
 
 // Internista Agusto Somarriba
 
-import asomarribaVideo1 from '../assets/Images/agustoSomarriba/reels/A.S- 1.mp4';
-import asomarribaVideo2 from '../assets/Images/agustoSomarriba/reels/A.S- 2.mp4';
-import asomarribaVideo3 from '../assets/Images/agustoSomarriba/reels/A.S- 3.mp4';
-import asomarribalogo from '../assets/Images/agustoSomarriba/logo.png';
+import asomarribaVideo1 from '/Images/agustoSomarriba/reels/A.S- 1.mp4';
+import asomarribaVideo2 from '/Images/agustoSomarriba/reels/A.S- 2.mp4';
+import asomarribaVideo3 from '/Images/agustoSomarriba/reels/A.S- 3.mp4';
+import asomarribalogo from '/Images/agustoSomarriba/logo.png';
 
 
 export const videos: VideoItem[] = [
